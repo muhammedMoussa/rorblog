202531a8e5f6c524784abc560ffbbf67db2b34ca
muhammedmoussa <muhammedmoussa@hotmail.com>
2018-09-07 14:53:17 +0200

    style edit and new post page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,9 +19,10 @@
 document.addEventListener("turbolinks:load", function() {
 	tinymce.remove()
 	tinymce.init({
-	  selector: "textarea#post_body",
-	  plugins: "codesample image media link code",
-	  toolbar: "undo redo | styleselect | bold italic link | codesample image media code",
-	  themes: "modern"
+		height: '470',
+		selector: "textarea#post_body",
+		plugins: "codesample image media link code",
+		toolbar: "undo redo | styleselect | bold italic link | codesample image media code",
+		themes: "modern"
 	});
 })
